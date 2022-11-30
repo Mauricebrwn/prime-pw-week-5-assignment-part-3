4492d88fcd3ee85939c3ffb5cd1ea99c9ef3db28
Maurice Brown <mauricebrwn6890@gmail.com>
2022-11-29 18:58:41 -0600

    function add collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,14 @@
 console.log('***** Music Collection *****')
 let collection = [];
+let album = {
+    title:'Nothing Was the Same',
+    artist:'Drake',
+    yearPublished:'2013'
+}
+console.log(collection);
+function addToCollection() {
+    collection.push(album)
+    return album;
+}
+addToCollection();
+addToCollection();
